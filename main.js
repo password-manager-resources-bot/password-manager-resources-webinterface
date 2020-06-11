7bdbf2100c433da6b01e35a753c84e61ed9b0ae7
DerJuulsn <julian.bildner@web.de>
2020-06-11 14:03:00 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,119 @@
+const {JSDOM} = require("jsdom");
+const {window} = new JSDOM("");
+const $ = require("jquery")(window);
+
+const Octokat = require("octokat");
+const token = "3549980cf6de5b15aa418e6b7b7256dca21c6509";
+const github = new Octokat({'token': token});
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => console.log("listen on port 3000!"));
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/api', (request, response) => {
+    console.log(request.body);
+    pullRequest("password-manager-resources-bot/password-manager-resources", request.body['url'], request.body['rule']);
+});
+
+function pullRequest(repositoryURL, url, rule) {
+
+    let repo = getRepo(repositoryURL);
+
+    $.getJSON('https://raw.githubusercontent.com/password-manager-resources-bot/password-manager-resources/master/quirks/password-rules.json', function (passwordRules) {
+
+        passwordRules[url] = {'password-rules': rule};
+
+        let passwordRulesSorted = {};
+        Object.keys(passwordRules).sort().forEach(each => {
+            passwordRulesSorted[each] = passwordRules[each];
+        });
+
+        repo.branches("master").fetch().then((data) => {
+
+            console.log("master branch found!");
+            let master_sha = data["commit"]["sha"];
+
+            repo.git.refs.create({
+                ref: "refs/heads/" + url,
+                sha: master_sha
+            }).then(() => {
+                console.log("Branch created!");
+
+                createCommit(repo, "quirks/password-rules.json", passwordRulesSorted, url)
+                    .then((b) => {
+                        setTimeout(() => {
+                            getRepo("password-manager-resources-bot/password-manager-resources").pulls.create({
+                                title: `Add website ${url} | This PR was created automatically, please ignore it until this feature is ready to use!`,
+                                body: "**Don't** pull these changes in, will be closed shortly.",
+                                head: "password-manager-resources-bot:" + url,
+                                base: "master" // change to master
+                            }).then(() => {
+                                console.log("PR created to " + repositoryURL);
+                            }).catch((err) => {
+                                console.log("Can't create PR!");
+                                console.log(err);
+                            });
+                        }, 4000);
+                    });
+            }).catch(() => {
+                console.log("Branch can't be created. It already exists.")
+            });
+        }).catch(() => {
+            console.log("Can't find master branch!");
+        });
+    });
+}
+
+const getRepo = (repositoryUrl) => {
+
+    const [user, repoName] = repositoryUrl.split('/');
+
+    if (user === null || repoName === null) {
+        console.log("Please specify a repo");
+        return;
+    }
+
+    return github.repos(user, repoName);
+}
+
+const createCommit = async (repo, filename, data, url) => {
+    try {
+        const path = filename.toLowerCase();
+        let main = await repo.git.refs("heads/" + url).fetch();
+        let treeItems = [];
+        let file = await repo.git.blobs.create({
+            content: Buffer.from(JSON.stringify(data, null, 4,)).toString('base64'),
+            encoding: 'base64'
+        });
+
+        treeItems.push({
+            path: path,
+            sha: file.sha,
+            mode: "100644",
+            type: "blob"
+        });
+
+        let tree = await repo.git.trees.create({
+            tree: treeItems,
+            base_tree: main.object.sha
+        });
+
+        let commit = await repo.git.commits.create({
+            //Commit message
+            message: `Created via Web - ${url}`,
+            tree: tree.sha,
+            parents: [main.object.sha]
+        });
+
+        main.update({sha: commit.sha})
+
+        console.log("Commited!");
+        return true;
+    } catch (err) {
+        console.error(err);
+        return false;
+    }
+}
---
+++ public/scripts/client.js
@@ -0,0 +1,20 @@
+function validateForm(){
+    //TODO: create rule from form
+
+    let url = document.getElementById("url").value;
+    let pic = document.getElementById("picture").value;
+
+    const data = {url: url, rule: "", picture: Buffer.from(pic).toString('base64')};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    fetch('/api', options).then(r => {
+        console.log(r);
+    });
+
+}
